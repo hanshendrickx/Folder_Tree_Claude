ec0736ce25cf0c3c387b8584077a93be7033cdd1
hanshendrickx <4386404+hanshendrickx@users.noreply.github.com>
2025-09-03 10:05:50 +0200

    Remove all commands from start_FT.bat

--- start_FT.bat
+++ start_FT.bat
@@ -1,11 +1,2 @@
-@echo off
-call C:\Users\hansh\envs\v0FFT\.venv\Scripts\activate.bat
-cd C:\Users\hansh\envs\v0FFT\v0_Free_Folder_Tree
-python folder_tree.py /path/to/your/project
-python folder_tree.py . --style artisanal --icons artisana
-python folder_tree.py . --style artisanal --icons artisanal --max-files 5 --depth 3
-python folder_tree.py . --icons artisanal --max-files 5 --depth 3
-python folder_tree.py . 5 beautify --icons artisanal --max-files 5
-python folder_tree.py . --icons artisanal --depth 2
-python folder_tree.py . --max-files 3
-python folder_tree.py . --style artisanal --icons artisanal --depth 4
+
+
